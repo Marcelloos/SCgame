2ddcfb627a2f2d55fae94040e88b835b6ec74f72
Marcell Oosthuizen <oosthuizenmarcell@gmail.com>
2019-10-17 13:50:02 +0200

    New Added changes
    Login pages

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
+
+
 @app.route("/")
+def index():
+    return render_template("index.html")
 
-def home():
-  return render_template("home.html")
 
-@app.route("/about")
-def about():
-  return render_template("about.html")
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("dorm"):
+        return render_template("failure.html")
+    with open("registered.csv", "a") as file:
+        writer = csv.DictWriter(file, fieldnames=["name", "dorm"])
+        writer.writerow({"name": request.form.get("name"), "dorm": request.form.get("dorm")})
+    return render_template("success.html")
 
-if __name__ == "__main__":
-  app.run(debug=True)
+
+@app.route("/registered")
+def registered():
+    with open("registered.csv", "r") as file:
+        reader = csv.DictReader(file)
+        students = list(reader)
+    return render_template("registered.html", students=students)
